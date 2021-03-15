1cfc1ea5d60509f1f70e33fb6fcad60c39c8accb
atctex <atctex@gmail.com>
2021-03-15 11:13:50 +0100

    first commit

---
+++ Readme.txt
@@ -0,0 +1,18 @@
+* About
+
+  Minimalistic Snowpack plugin for MDX with React.
+
+* Usage
+
+  Inside Snowpack config:
+
+    plugins: [
+      "mdx-snowpack-plugin",
+      // ...
+    ],
+  
+  Now you are ready to process .mdx files.
+
+* License
+
+  MIT License
---
+++ index.js
@@ -0,0 +1,29 @@
+const fs_async = require('fs').promises
+const mdx = require('@mdx-js/mdx')
+const babel = require('@babel/core')
+
+const setup = {
+  name: 'mdx-babel-plugin',
+  babelPresets: ['@babel/preset-react'],
+  mdx: '@mdx-js/react',
+}
+
+module.exports = function (cfg, opts) {
+  return {
+    name: setup.name,
+    knownEntrypoints: [setup.mdx],
+    resolve: { input: ['.md', '.mdx'], output: ['.js'], },
+
+    async load({ filePath }) {
+      const babelConf = babel.loadPartialConfig({presets: setup.babelPresets})
+
+      const mdxContent = await fs_async.readFile(filePath, 'utf-8')
+      const jsxContent = `import { mdx } from '${setup.mdx}';\n${await mdx(mdxContent)}`
+      const jsContent = await babel.transformAsync(jsxContent, babelConf.options)
+
+      return {
+        '.js': { code: jsContent.code || ''}
+      }
+    },
+  };
+};
